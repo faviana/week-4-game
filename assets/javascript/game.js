913c45b05155adbd94ad5bb1a9e69ad5f745ee43
faviana lopez <ms.favina@gmail.com>
2017-11-16 12:32:57 -0500

    consul doesn't recognize jquery added images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
-//create an alert to verify pages have references
 
-alert("hi");
+//anonymous function that runs once the DOM is ready for JavaScript code to execute
+$( document ).ready(function(){
+    // create a random number between 10-120
+    var createRandomNumber=Math.floor(Math.random()*101+19)
+    //createRandomNumber is added to the randomNumber id div
+    $('#randomNumber').text(createRandomNumber);
+});
+
