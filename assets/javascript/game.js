f2b41474371e0c1f05ecc065e294a99d39a5591d
faviana lopez <ms.favina@gmail.com>
2017-11-16 17:20:04 -0500

    reset is implemented and created function so code is not repeated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,34 +2,37 @@
 $(document).ready(function () {
     // create a random number between 10-120
     var createRandomNumber = Math.floor(Math.random() * 101 + 19)
-    //createRandomNumber is added to the randomNumber id div
-    $('#randomNumber').text(createRandomNumber);
+    //createRandomNumber is added to the InitialRandomNumber id div
+    $('#InitialRandomNumber').text(createRandomNumber);
 
-    //assign random numbers (1-12) to the images
+    //assign random values (1-12) to the images
     var randomPointNumber1 = Math.floor(Math.random() * 11 + 1)
     var randomPointNumber2 = Math.floor(Math.random() * 11 + 1)
     var randomPointNumber3 = Math.floor(Math.random() * 11 + 1)
     var randomPointNumber4 = Math.floor(Math.random() * 11 + 1)
 
+    //declare variables
     var totalPoint = 0;
     var wins = 0;
     var losses = 0;
 
 
-    //randomPointNumber1 is assigned to blueGeenL image
-    $('#blueGreenL').on ('click', function(){
-
-        //calculates total points while clicking
-        totalPoint = totalPoint + randomPointNumber1;
-        //displays the total points so far
-        $('#totalValue').text(totalPoint); 
-
+    function winOrLose(){
         //if created random number equals total points
         if(totalPoint == createRandomNumber){
             // you win, add one point to wins
             wins++;
             //displays the winning point++
             $('#win').text(wins);
+
+           //assign new random value
+            createRandomNumber=Math.floor(Math.random()*101+19);
+            $('#InitialRandomNumber').text(createRandomNumber);
+        
+            //total points equal to zero
+            totalPoint = 0;
+            $('#totalValue').text(totalPoint);
+
         }
         
         //if totalPoint is geated than the random number
@@ -38,8 +41,28 @@ $(document).ready(function () {
             losses++;
             //displays the lossing point++
             $('#lose').text(losses);
+
+            //assign new random value
+            createRandomNumber=Math.floor(Math.random()*101+19);
+            $('#InitialRandomNumber').text(createRandomNumber);
+        
+            //total points equal to zero
+            totalPoint = 0;
+            $('#totalValue').text(totalPoint);
         }
-       
+
+    }
+
+   
+    //randomPointNumber1 is assigned to blueGeenL image
+    $('#blueGreenL').on ('click', function(){
+
+        //calculates total points while clicking
+        totalPoint = totalPoint + randomPointNumber1;
+        //displays the total points so far
+        $('#totalValue').text(totalPoint); 
+
+        winOrLose()  
     });
 
     //randomPointNumber2 is assigned to blueGreen image
@@ -50,23 +73,7 @@ $(document).ready(function () {
         //displays the total points so far
         $('#totalValue').text(totalPoint);
         
-        //if created random number equals total points
-        if(totalPoint == createRandomNumber){
-            // you win, add one point to wins
-            wins++;
-            //displays the winning point++
-            $('#win').text(wins);
-
-        }
-        
-        //if totalPoint is greated than the random number
-        else if(totalPoint>createRandomNumber){
-            //you loose, add one point to losses
-            losses++;
-            //displays the lossing point++
-            $('#lose').text(losses);
-
-         }
+        winOrLose()
  
      });
 
@@ -78,21 +85,7 @@ $(document).ready(function () {
         //displays the total points so far
         $('#totalValue').text(totalPoint);
         
-        //if created random number equals total points
-        if(totalPoint == createRandomNumber){
-            // you win, add one point to wins
-            wins++;
-            //displays the winning point++
-            $('#win').text(wins);
-        }
-        
-        //if totalPoint is greated than the random number
-        else if(totalPoint>createRandomNumber){
-            //you loose, add one point to losses
-            losses++;
-            //displays the lossing point++
-            $('#lose').text(losses);
-        }
+        winOrLose()        
  
      });
 
@@ -104,21 +97,7 @@ $(document).ready(function () {
         //displays the total points so far
         $('#totalValue').text(totalPoint);
         
-        //if created random number equals total points
-        if(totalPoint == createRandomNumber){
-            // you win, add one point to wins
-            wins++;
-            //displays the winning point++
-            $('#win').text(wins);
-        }
-        
-        //if totalPoint is greated than the random number
-        else if(totalPoint>createRandomNumber){
-            //you loose, add one point to losses
-            losses++;
-            //displays the lossing point++
-            $('#lose').text(losses);
-        }
+        winOrLose()
  
      });
 
