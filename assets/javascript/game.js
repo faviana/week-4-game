331f6d5b50dd83cabb8430bb71805687ff11b77b
faviana lopez <ms.favina@gmail.com>
2017-11-16 15:41:54 -0500

    your value, wins, losses display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,41 +11,104 @@ $(document).ready(function () {
     var randomPointNumber3 = Math.floor(Math.random() * 11 + 1)
     var randomPointNumber4 = Math.floor(Math.random() * 11 + 1)
 
-    totalPoint = 0;
+    var totalPoint = 0;
+    var wins = 0;
+    var losses = 0;
+
 
     //randomPointNumber1 is assigned to blueGeenL image
     $('#blueGreenL').on ('click', function(){
 
+        //calculates total points while clicking
         totalPoint = totalPoint + randomPointNumber1;
-        console.log("New totalPoint= " + totalPoint);
+        //displays the total points so far
         $('#totalValue').text(totalPoint); 
+
+        //if created random number equals total points
+        if(totalPoint == createRandomNumber){
+            // you win, add one point to wins
+            wins++;
+            //displays the winning point++
+            $('#win').text(wins);
+        }
+        
+        else{
+            losses++;
+            //displays the lossing point++
+            $('#lose').text(losses);
+        }
        
     });
 
     //randomPointNumber2 is assigned to blueGreen image
     $('#blueGreen').on ('click', function(){
-        
+
+        //calculates total points while clicking        
         totalPoint = totalPoint + randomPointNumber2;
-        console.log("New totalPoint= " + totalPoint);
-        $('#totalValue').text(totalPoint); 
+        //displays the total points so far
+        $('#totalValue').text(totalPoint);
+        
+        //if created random number equals total points
+        if(totalPoint == createRandomNumber){
+            // you win, add one point to wins
+            wins++;
+            //displays the winning point++
+            $('#win').text(wins);
+        }
+        
+        else{
+            losses++;
+            //displays the lossing point++
+            $('#lose').text(losses);
+        }
  
      });
 
     //randomPointNumber3 is assigned to coral image  
      $('#coral').on ('click', function(){
-        
+
+        //calculates total points while clicking        
         totalPoint = totalPoint + randomPointNumber3;
-        console.log("New totalPoint= " + totalPoint);
-        $('#totalValue').text(totalPoint); 
+        //displays the total points so far
+        $('#totalValue').text(totalPoint);
+        
+        //if created random number equals total points
+        if(totalPoint == createRandomNumber){
+            // you win, add one point to wins
+            wins++;
+            //displays the winning point++
+            $('#win').text(wins);
+        }
+        
+        else{
+            losses++;
+            //displays the lossing point++
+            $('#lose').text(losses);
+        }
  
      });
 
      //randomPointNumber4 is assigned to mustard image
      $('#mustard').on ('click', function(){
-        
+
+        //calculates total points while clicking        
         totalPoint = totalPoint + randomPointNumber4;
-        console.log("New totalPoint= " + totalPoint);
-        $('#totalValue').text(totalPoint); 
+        //displays the total points so far
+        $('#totalValue').text(totalPoint);
+        
+        //if created random number equals total points
+        if(totalPoint == createRandomNumber){
+            // you win, add one point to wins
+            wins++;
+            //displays the winning point++
+            $('#win').text(wins);
+        }
+        
+        else{
+            losses++;
+            //displays the lossing point++
+            $('#lose').text(losses);
+        }
  
      });
 
